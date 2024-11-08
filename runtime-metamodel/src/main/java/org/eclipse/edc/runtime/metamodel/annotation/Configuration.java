45e2a8d5a6be1a815378734e9ce8f771be189cee
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-11-08 12:16:27 +0100

    Update Configuration.java
    Co-authored-by: Jim Marino <jim.marino@gmail.com>

--- runtime-metamodel/src/main/java/org/eclipse/edc/runtime/metamodel/annotation/Configuration.java
+++ runtime-metamodel/src/main/java/org/eclipse/edc/runtime/metamodel/annotation/Configuration.java
@@ -21,7 +21,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation indicates that a certain field is a "configuration object", i.e. a POJO that contains fields annotated with
+ * Indicates that a certain field is a "configuration object", i.e. a POJO that contains fields annotated with
  * {@link Setting}. These fields must be declared inside an extension class. Their respective class declaration must be annotated
  * with {@link Settings}.
  * Types annotated with this annotation will get instantiated by the EDC dependency injection mechanism using values from the config.
