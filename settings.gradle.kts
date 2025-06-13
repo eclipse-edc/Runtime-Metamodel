e2a669f95109c76a2010cf8570adecb54fa2446f
ndr_brt <andrea.bertagnolli@gmail.com>
2025-06-13 09:20:18 +0200

    build: update plugin snapshot repository url

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,12 @@ rootProject.name = "runtime-metamodel"
 include(":runtime-metamodel")
 include(":version-catalog")
 
-// this is needed to have access to snapshot builds of plugins
 pluginManagement {
     repositories {
         maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
         }
         mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositories {
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-        mavenCentral()
-        mavenLocal()
     }
 }
