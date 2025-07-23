daf67e1c3600fb9990481b44c5dea0d3b8e9b68f
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2025-07-23 15:38:27 +0200

    build: use edc-build from Gradle Plugin Portal (#86)
    build(deps): Bump jackson from 2.19.1 to 2.19.2 (#85)
    
    Bumps `jackson` from 2.19.1 to 2.19.2.
    
    Updates `com.fasterxml.jackson.core:jackson-annotations` from 2.19.1 to 2.19.2
    - [Commits](https://github.com/FasterXML/jackson/commits)
    
    Updates `com.fasterxml.jackson.core:jackson-databind` from 2.19.1 to 2.19.2
    - [Commits](https://github.com/FasterXML/jackson/commits)
    
    ---
    updated-dependencies:
    - dependency-name: com.fasterxml.jackson.core:jackson-annotations
      dependency-version: 2.19.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: com.fasterxml.jackson.core:jackson-databind
      dependency-version: 2.19.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,14 @@
 plugins {
     `java-library`
+    alias(libs.plugins.edc.build)
 }
 
 val edcScmUrl: String by project
 val edcScmConnection: String by project
-val edcBuildVersion = libs.versions.edc.build
-
-buildscript {
-    dependencies {
-        classpath(libs.edc.build) {
-            exclude("org.jetbrains", "annotations")
-            exclude("org.jetbrains.kotlin", "kotlin-stdlib")
-        }
-    }
-}
+val edcBuildId = libs.plugins.edc.build.get().pluginId
 
 allprojects {
-    apply(plugin = "${group}.edc-build")
-
-    configure<org.eclipse.edc.plugins.autodoc.AutodocExtension> {
-        processorVersion.set(edcBuildVersion)
-        outputDirectory.set(project.layout.buildDirectory.asFile)
-    }
+    apply(plugin = edcBuildId)
 
     configure<org.eclipse.edc.plugins.edcbuild.extensions.BuildExtension> {
         pom {
